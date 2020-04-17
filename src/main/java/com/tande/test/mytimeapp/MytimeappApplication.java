e70515e059ce69c45194969a561e0b3007991025
omoelyon <ganiyuelegbede@yahoo.com>
2020-04-17 16:58:14 +0100

    my first timeapp commit

---
+++ src/main/java/com/tande/test/mytimeapp/MytimeappApplication.java
@@ -0,0 +1,13 @@
+package com.tande.test.mytimeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytimeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytimeappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tande/test/mytimeapp/MytimeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tande.test.mytimeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytimeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
